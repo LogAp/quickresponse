3a6b358cbe4aa34177f09fe846ab75548f20f0ce
Jonathan Batista <jonathan.b.d.o.s@gmail.com>
2018-02-05 23:51:28 -0300

    Disable browser cache

---
+++ src/main/java/me/jaybios/quickresponse/middleware/NoCacheFilter.java
@@ -0,0 +1,32 @@
+package me.jaybios.quickresponse.middleware;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class NoCacheFilter implements Filter {
+    private FilterConfig config;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        httpResponse.setHeader("Pragma", "no-cache");
+        httpResponse.setDateHeader("Expires", 0);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        config = filterConfig;
+    }
+
+    @Override
+    public void destroy() {
+        config = null;
+    }
+}
